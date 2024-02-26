e3418b2ff881be36eabdf1e103afcd08b69d2094
Lorenz Lamm <Lorenz.Lamm@helmholtz-muenchen.de>
2024-02-26 20:39:03 +0100

    Factor out functions

--- examples/pixel_embedding/merge_point_annotations.py
+++ examples/pixel_embedding/merge_point_annotations.py
@@ -45,6 +45,56 @@ def insert_spheres(data, coordinates, sphere_radius, sphere_value):
     return data
 
 
+def load_any_mrc_file(directory):
+    """ Load the first mrc file in the directory."""
+    for filename in os.listdir(directory):
+        print(filename)
+        if filename.endswith(".mrc"):
+            with mrcfile.open(os.path.join(directory, filename), mode="r", permissive=True) as mrc:
+                data = mrc.data.copy()
+                return data
+            
+
+def get_protein_name(filename):
+    """ Extract the protein name from the filename."""
+    for protein in labels.keys():
+        if protein in filename:
+            return protein
+    return None
+
+
+def load_point_annotations_from_ndjson(filename):
+    """ Load the point annotations from an ndjson file."""
+    coordinates = []
+    with open(filename, "r") as f:
+        for line in f:
+            # Parse the JSON object from each line
+            data = json.loads(line)
+            
+            # Extract the location object
+            location = data.get('location', {})
+            
+            # Extract x, y, z coordinates
+            x = location.get('x')
+            y = location.get('y')
+            z = location.get('z')
+            
+            # # Append the coordinates to the list
+            coordinates.append((x, y, z))
+    return np.array(coordinates)
+
+
+def save_mrc_file(output_filename, data):
+    """ Save the data to an mrc file. 
+    
+    Header information is copied from the original file.
+    """
+    with mrcfile.new(output_filename, overwrite=True) as mrc:
+        mrc.set_data(data)
+        mrc.close()
+
+
+
 ## Assumes folder structure:
 # data
 # ├── annotations
@@ -72,46 +122,20 @@ for dataset_name in os.listdir(annotations_directory):
     )
 
     # Load the first annotation (all annotation files contain the entire segmentation data)
-    for filename in os.listdir(tomo_annotations_directory):
-        if filename.endswith(".mrc") and not filename == "merged_point_annotations.mrc":
-            # Load the annotation
-            with mrcfile.open(os.path.join(tomo_annotations_directory, filename), mode="r", permissive=True) as mrc:
-                # load the annotation data
-                annotation = mrc.data.copy()
-
-                # Transpose the data to match the coordinates
-                annotation = np.transpose(annotation, (2, 1, 0))
-            break
+    annotation = load_any_mrc_file(tomo_annotations_directory)
+    
+    # Transpose the data to match the coordinates
+    annotation = np.transpose(annotation, (2, 1, 0))
 
     # Iterate over the annotation files
     for filename in os.listdir(tomo_annotations_directory):
 
         if filename.endswith(".ndjson"):
             # Extract the protein name from the filename
-            for protein in labels.keys():
-                if protein in filename:
-                    break
+            protein = get_protein_name(filename)
             
             # Load the point annotations
-            coordinates = []
-            with open(os.path.join(tomo_annotations_directory, filename), "r") as f:
-                for line in f:
-                    # Parse the JSON object from each line
-                    data = json.loads(line)
-                    
-                    # Extract the location object
-                    location = data.get('location', {})
-                    
-                    # Extract x, y, z coordinates
-                    x = location.get('x')
-                    y = location.get('y')
-                    z = location.get('z')
-                    
-                    # # Append the coordinates to the list
-                    coordinates.append((x, y, z))
-            
-            # Convert the coordinates to a numpy array
-            coordinates = np.array(coordinates)
+            coordinates = load_point_annotations_from_ndjson(os.path.join(tomo_annotations_directory, filename))
 
             # Insert the spheres into the annotation volume
             annotation = insert_spheres(
@@ -125,9 +149,4 @@ for dataset_name in os.listdir(annotations_directory):
     output_filename = os.path.join(tomo_annotations_directory, "merged_point_annotations.mrc")
     annotation = np.transpose(annotation, (2, 1, 0))
 
-    # Copying and changing the annotation keeps the original header information
-    shutil.copyfile(os.path.join(tomo_annotations_directory, filename), output_filename)
-    # change data to the new annotation
-    with mrcfile.open(output_filename, mode="r+") as mrc:
-        mrc.set_data(annotation)
-        mrc.close()
+    save_mrc_file(output_filename, annotation, os.path.join(tomo_annotations_directory, filename))
